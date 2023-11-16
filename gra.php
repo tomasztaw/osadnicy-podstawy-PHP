3aa417ba0c7a270ecafaf49e3b7b08ab9b50cd70
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-11-16 15:05:16 +0100

    odcinek 4 data i czas na serwerze

--- gra.php
+++ gra.php
@@ -25,9 +25,21 @@
     echo " | <b>Zboże</b>: ".$_SESSION['zboze']."</p>";
 
     echo "<p><b>E-mail</b>: ".$_SESSION['email'];
-    echo "<br/><b>Dni premium</b>: ".$_SESSION['dnipremium']."</p>";
+    echo "<br/><b>Data wygaśnięcia premium</b>: ".$_SESSION['dnipremium']."</p>";
 
-?>
+    $dataczas = new DateTime('2150-05-01 09:33:59');
+
+    echo "Data i czas serwera: ".$dataczas->format('Y-m-d H:i:s')."<br>";
+    
+    $konice = DateTime::createFromFormat('Y-m-d H:i:s', $_SESSION['dnipremium']);
+
+    $roznica = $dataczas->diff($konice);
+
+    if ($dataczas < $konice)
+        echo "Pozostało premium: ".$roznica->format('%y lat, %m mies, %d dni, %h godz, %i min, %s sek');
+    else
+    echo "Premium nie aktywane od: ".$roznica->format('%y lat, %m mies, %d dni, %h godz, %i min, %s sek');
+    ?>
 
 </body>
 
--- rejestracja.php
+++ rejestracja.php
@@ -117,7 +117,7 @@
                 if ($wszystko_OK==true)
                 {
                     // Hurra, wszystkie testy zaliczone, dodajemy gracza do bazy
-                    if ($polaczenie->query("INSERT INTO uzytkownicy VALUES (NULL, '$nick', '$haslo_hash', '$email', 100, 100, 100, 14)"))
+                    if ($polaczenie->query("INSERT INTO uzytkownicy VALUES (NULL, '$nick', '$haslo_hash', '$email', 100, 100, 100, now() + INTERVAL 14 DAY)"))
                     {
                         $_SESSION['udanarejestracja']=true;
                         header('Location: witamy.php');
